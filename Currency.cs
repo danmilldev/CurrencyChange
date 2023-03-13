a7b106bdc5618f7b0d125b8b76119f2003fd4dea
danmilldev <silbercrisis63@gmail.com>
2023-03-13 17:34:47 +0100

    Added Currency Changing Methods

---
+++ Currency.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyChange
+{
+    public class Currency
+    {
+        void PrintOldCurrency(ref decimal currency, ref string currencyName) => Console.WriteLine("Old Currency From: " + currency + " " + currencyName);
+
+        public void ChangeToEuro(ref decimal currency,ref string signature)
+        {
+
+            PrintOldCurrency(ref currency, ref signature);
+
+            if (signature.Equals("USD")) //From USD
+            {
+                currency *= 0.93m;
+            }
+            else if(signature.Equals("JPY")) //FROM JAPANESE YEN
+            {
+                currency *= 0.007m;
+            }
+
+            signature = "EURO";
+
+            Console.WriteLine("Current Currency : " + currency + " " + signature);
+            Console.WriteLine();
+        }
+
+        public void ChangeToUSD(ref decimal currency,ref string signature)
+        {
+            PrintOldCurrency(ref currency, ref signature);
+
+            if (signature.Equals("EURO")) //From EURO
+            {
+                currency *= 1.07m;
+            }
+            else if (signature.Equals("JPY")) //FROM JAPANESE YEN
+            {
+                currency *= 0.0075m;
+            }
+
+            signature = "USD";
+
+            Console.WriteLine("Current Currency : " + currency + " " + signature);
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CurrencyChange;
+
+decimal money = 50.74m;
+string moneySignature = "USD";
+
+Currency currency = new();
+
+currency.ChangeToEuro(ref money,ref moneySignature);
+
+currency.ChangeToUSD(ref money,ref moneySignature);
